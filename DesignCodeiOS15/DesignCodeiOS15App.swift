215510e10fa2ce9f36323f2516088d17a14ee737
ch8ser <we@thech8sers.com>
2022-07-15 10:29:04 -0400

    Initial Commit

---
+++ DesignCodeiOS15/DesignCodeiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15App.swift
+//  DesignCodeiOS15
+//
+//  Created by The Ch8sers Inc. on 7/15/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
