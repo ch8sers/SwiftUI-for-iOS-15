a1b4ecf6c0d077f6fe78701eaa980df90e9f1739
ch8ser <we@thech8sers.com>
2022-07-15 11:28:56 -0400

    • Add additional modifiers outside of Visual Editor • Add Previews to see changes with Light and Dark Mode

--- DesignCodeiOS15/ContentView.swift
+++ DesignCodeiOS15/ContentView.swift
@@ -9,18 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(alignment: .leading, spacing: 8.0) {
+            Spacer()
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+                .cornerRadius(20.0)
             Text("SwiftUI for iOS 15")
                 .font(.largeTitle)
-            .fontWeight(.bold)
-            Text(/*@START_MENU_TOKEN@*/"20 sections - 3 hours"/*@END_MENU_TOKEN@*/)
+                .fontWeight(.bold)
+            Text("20 sections - 3 hours".uppercased())
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .foregroundColor(.secondary)
+            Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+                .font(.footnote)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(.secondary)
         }
+        .padding(.all, 20.0)
+        .padding(.vertical, 20.0)
+        .frame(height: 350.0)
+        .background(Color("Background"))
+        .cornerRadius(30.0)
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .padding(.horizontal, 20.0)
             
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+            
+            
+            
+            
     }
 }
