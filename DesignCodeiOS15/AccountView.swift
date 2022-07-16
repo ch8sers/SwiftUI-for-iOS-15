3d72b4c14b780dff1eec2027b405c7aa1b7380c7
ch8ser <we@thech8sers.com>
2022-07-16 10:32:37 -0400

    * Make your code cleaner by dividing into reusable sections * Revise code structure to make it more readable * Toggle states to provide more swipe actions to list * Use the new Canvas API in SwiftUI to draw a Path, Text, Image or SF Symbol. * Learn how to style the resulting drawing. * Animate blob based off of angle and degrees * Start  blob animation and apply timing and looping * Add BlobView to Account View / Profile Tab

--- DesignCodeiOS15/AccountView.swift
+++ DesignCodeiOS15/AccountView.swift
@@ -9,88 +9,16 @@ import SwiftUI
 
 struct AccountView: View {
     @State var isDeleted = false
+    @State var isPinned = false
     
     var body: some View {
         NavigationView {
             List {
-                VStack(spacing: 8) {
-                    Image(systemName:
-                            "person.crop.circle.fill.badge.checkmark")
-                        .symbolVariant(.circle.fill)
-                        .font(.system(size: 32))
-                        .symbolRenderingMode(.palette)
-                        .foregroundStyle(.blue, .blue.opacity(0.3))
-                        .padding()
-                        .background(Circle().fill(.ultraThinMaterial))
-                        .background(
-                            Image(systemName: "hexagon")
-                                .symbolVariant(.fill)
-                                .foregroundColor(.blue)
-                                .font(.system(size: 200))
-                                .offset(x: -50, y: -100)
-                    )
-                    Text("Ch8se Tech")
-                        .font(.title.weight(.semibold))
-                    HStack {
-                        Image(systemName: "location")
-                            .imageScale(.small)
-                        Text("Delaware")
-                            .foregroundColor(.secondary)
-                    }
-                }
-                .frame(maxWidth: .infinity)
+                profile
                 
-                Section {
-                    NavigationLink(destination: ContentView()) {
-                        Label("Settings", systemImage: "gear")
-                    }
-                    
-                    NavigationLink { Text("Billing")} label: {
-                        Label("Billing", systemImage: "creditcard")
-                    }
-                    
-                    NavigationLink { ContentView() } label: {
-                        Label("Help", systemImage: "questionmark")
-                    }
-                }
-                .accentColor(.primary)
-                .listRowSeparatorTint(.blue)
-                .listRowSeparator(.hidden)
+                menu
                 
-                    Section {
-                        if !isDeleted {
-                        Link(destination: URL(string: "https://apple.com")!) {
-                            HStack {
-                                Label("Website", systemImage: "house")
-                                Spacer()
-                                Image(systemName: "link")
-                                    .foregroundColor(.secondary)
-                            }
-                        }
-                        .swipeActions(edge: .leading, allowsFullSwipe: true) {
-                            Button(action: { isDeleted = true }) {
-                                Label("Delete", systemImage: "trash")
-                            }
-                            .tint(.red)
-                            
-                            Button {} label: {
-                                Label("Pin", systemImage: "pin")
-                            }
-                            .tint(.yellow)
-                            }
-                        }
-                        
-                        Link(destination: URL(string: "https://ch8setech.com")!) {
-                            HStack {
-                                Label("Portfolio", systemImage: "dpad.down.filled")
-                                Spacer()
-                                Image(systemName: "link")
-                                    .foregroundColor(.secondary)
-                            }
-                        }
-                    }
-                    .accentColor(.primary)
-                .listRowSeparator(.hidden)
+                links
                 
                 
             }
@@ -98,14 +26,112 @@ struct AccountView: View {
             .navigationTitle("Account")
         }
     }
+    
+    var profile: some View {
+        VStack(spacing: 8) {
+            Image(systemName:
+                    "person.crop.circle.fill.badge.checkmark")
+                .symbolVariant(.circle.fill)
+                .font(.system(size: 32))
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(.blue, .blue.opacity(0.3))
+                .padding()
+                .background(Circle().fill(.ultraThinMaterial))
+                .background(
+                    HexagonView()
+                        .offset(x: -50, y: -100)
+            )
+                .background(
+                    BlobView()
+                        .offset(x:200, y:0)
+                        .scaleEffect(0.7)
+                )
+            Text("Ch8se Tech")
+                .font(.title.weight(.semibold))
+            HStack {
+                Image(systemName: "location")
+                    .imageScale(.small)
+                Text("Delaware")
+                    .foregroundColor(.secondary)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+    }
+    
+    var menu: some View {
+        Section {
+            NavigationLink(destination: ContentView()) {
+                Label("Settings", systemImage: "gear")
+            }
+            
+            NavigationLink { Text("Billing")} label: {
+                Label("Billing", systemImage: "creditcard")
+            }
+            
+            NavigationLink { ContentView() } label: {
+                Label("Help", systemImage: "questionmark")
+            }
+        }
+        .accentColor(.primary)
+        .listRowSeparatorTint(.blue)
+        .listRowSeparator(.hidden)
+    }
+    
+    var links: some View {
+        Section {
+            if !isDeleted {
+            Link(destination: URL(string: "https://apple.com")!) {
+                HStack {
+                    Label("Website", systemImage: "house")
+                    Spacer()
+                    Image(systemName: "link")
+                        .foregroundColor(.secondary)
+                }
+            }
+            .swipeActions(edge: .leading, allowsFullSwipe: true) {
+                Button(action: { isDeleted = true }) {
+                    Label("Delete", systemImage: "trash")
+                }
+                .tint(.red)
+                pinButton
+                }
+            }
+            
+            Link(destination: URL(string: "https://ch8setech.com")!) {
+                HStack {
+                    Label("Portfolio", systemImage: "dpad.down.filled")
+                    Spacer()
+                    Image(systemName: "link")
+                        .foregroundColor(.secondary)
+                }
+            }
+            .swipeActions {
+                pinButton
+            }
+        }
+        .accentColor(.primary)
+    .listRowSeparator(.hidden)
+    }
+    
+    var pinButton: some View {
+        Button { isPinned.toggle() } label: {
+            if isPinned {
+                Label("Unpin", systemImage: "pin.slash")
+            } else {
+                Label("Pin", systemImage: "pin")
+            }
+            
+        }
+        .tint( isPinned ? .gray : .yellow)
+    }
 }
 
 struct AccountView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             AccountView()
-            AccountView()
-                .preferredColorScheme(.dark)
+//                .preferredColorScheme(.dark)
         }
     }
 }
