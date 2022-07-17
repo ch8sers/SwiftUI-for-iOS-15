c79bb25b6614186be0b3608c95170e0a9c16a916
ch8ser <we@thech8sers.com>
2022-07-17 03:07:34 -0400

    * Use Binding to synchronize your states between views to animate a custom Navigation Bar * Binding allows you to synchronize states between parent and child views. * When you're working with multiple views, it's helpful to make a Preview sticky while you jump back and forth between the files. * Create a custom animatable modifier to smoothly animate a text size * Create a beautiful horizontal scroll using tabViewStyle page * Create a Course file in the Model folder and set the data model and sample data.

---
+++ DesignCodeiOS15/Components/FeaturedItem.swift
@@ -0,0 +1,66 @@
+//
+//  FeaturedItem.swift
+//  DesignCodeiOS15
+//
+//  Created by The Ch8sers Inc. on 7/17/22.
+//
+
+import SwiftUI
+
+struct FeaturedItem: View {
+    var course: Course = courses[0]
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8.0) {
+            Spacer()
+            Image(course.logo)
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+                .cornerRadius(10.0)
+                .padding(9)
+                .background(.ultraThinMaterial, in:
+                        RoundedRectangle(cornerRadius: 16, style:
+                        .continuous))
+                .strokeStyle(cornerRadius: 16.0)
+            Text(course.title)
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(.linearGradient(colors: [.primary,.primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+            Text(course.subtitle.uppercased())
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .foregroundStyle(.secondary)
+            Text(course.text)
+                .font(.footnote)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(.secondary)
+                
+        }
+        .padding(.all, 20.0)
+        .padding(.vertical, 20.0)
+        .frame(height: 350.0)
+        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+//        .cornerRadius(30.0)
+//        .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .strokeStyle()
+//        .modifier(StrokeStyle())
+        .padding(.horizontal, 20.0)
+        .overlay(
+            Image(course.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 230)
+                .offset(x:32, y: -80)
+    )
+    }
+}
+
+struct FeaturedItem_Previews: PreviewProvider {
+    static var previews: some View {
+        FeaturedItem()
+    }
+}
--- DesignCodeiOS15/Navigation/ContentView.swift
+++ DesignCodeiOS15/Navigation/ContentView.swift
@@ -36,6 +36,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
             .previewInterfaceOrientation(.portrait)
     }
 }
