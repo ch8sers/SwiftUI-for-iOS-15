55e1904eed47ca28ec9a5f24113d995d71c34df9
ch8ser <we@thech8sers.com>
2022-07-18 14:17:11 -0400

    * guard allows you to set a condition, except that it won’t run the code after this line if that condition doesn’t pass. * Limit the swipe zone and add a dismiss animation with distance configurations * Apply a grid layout that spread items vertically and horizontally

--- DesignCodeiOS15/Views/MatchedView.swift
+++ DesignCodeiOS15/Views/MatchedView.swift
@@ -14,51 +14,12 @@ struct MatchedView: View {
     var body: some View {
         ZStack {
             if !show {
-                VStack(alignment: .leading, spacing: 12) {
-                    Text("SwiftUI")
-                        .font(.largeTitle.weight(.bold))
-                        .matchedGeometryEffect(id: "title", in: namespace)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    Text("20 Sections - 3 Hours".uppercased())
-                        .font(.footnote.weight(.semibold))
-                        .matchedGeometryEffect(id: "subtitle", in: namespace)
-                    Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
-                        .font(.footnote)
-                        .matchedGeometryEffect(id: "text", in: namespace)
-                }
-                .padding(20)
-                .foregroundStyle(.white)
-                .background(
-                    Color.red.matchedGeometryEffect(id: "background", in: namespace)
-                )
-                .padding(20)
+                
             } else {
-                VStack(alignment: .leading, spacing: 12) {
-                    Spacer()
-                    Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
-                        .font(.footnote)
-                        .matchedGeometryEffect(id: "text", in: namespace)
-                    Text("20 Sections - 3 Hours".uppercased())
-                        .font(.footnote.weight(.semibold))
-                        .matchedGeometryEffect(id: "subtitle", in: namespace)
-                    Text("SwiftUI")
-                        .font(.largeTitle.weight(.bold))
-                        .matchedGeometryEffect(id: "title", in: namespace)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                }
-                .padding(20)
-                .foregroundStyle(.black)
-                .background(Color.blue.matchedGeometryEffect(id: "background", in: namespace)
-                )
+                
             }
         }
-        .onTapGesture {
-            withAnimation(.spring(response: 0.6, dampingFraction: 0.8)) {
-                show.toggle()
-            }
-            
-        }
+        
     }
 }
 
