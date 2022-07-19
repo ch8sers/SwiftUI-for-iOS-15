c05ac4ce832834ded9d4f412ca9f04b320d8e700
ch8ser <we@thech8sers.com>
2022-07-19 11:08:04 -0400

    * Create a text field and secure field with customizations and a reusable style

--- DesignCodeiOS15/Views/SignUpView.swift
+++ DesignCodeiOS15/Views/SignUpView.swift
@@ -8,21 +8,53 @@
 import SwiftUI
 
 struct SignUpView: View {
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
         VStack (alignment: .leading, spacing: 16) {
             Text("Sign Up")
                 .font(.largeTitle).bold()
             Text("Access 120+ hours of courses, tutorials and livestreams")
                 .font(.headline)
+            TextField("Email", text: $email)
+                .inputStyle(icon: "mail")
+                .textContentType(.emailAddress)
+                .keyboardType(.emailAddress)
+                .autocapitalization(.none)
+                .disableAutocorrection(true)
+            SecureField("Password", text: $password)
+                .inputStyle(icon: "lock")
+                .textContentType(.password)
             Button {} label: {
                 Text("Create An Account")
                     .frame(maxWidth: .infinity)
                 
             }
             .font(.headline)
+            .blendMode(.overlay)
             .buttonStyle(.angular)
             .tint(.accentColor)
             .controlSize(.large)
+            
+            Group {
+                Text("By clicking on .")
+                + Text("__Create an Account__").foregroundColor(.primary.opacity(0.7))
+                + Text(", you agree to our **Terms of Service** and **[Privacy Policy](https://designcode.io)**")
+                
+                Divider()
+                
+                HStack {
+                    Text("Already have an Account?")
+                    Button {} label: {
+                        Text("**Sign In**")
+                    }
+                }
+            }
+            .font(.footnote)
+            .foregroundColor(.secondary)
+            .accentColor(.secondary)
+            
         }
         .padding(20)
         .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
